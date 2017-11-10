701a13c5f394b8c153f83497a4f88492421297ce
brooke8henderson <31804715+brooke8henderson@users.noreply.github.com>
2017-11-10 00:41:07 -0500

    fixed for mobile
    functions working, toggle and switch and hide and show

--- js/myscript.js
+++ js/myscript.js
@@ -3,6 +3,8 @@ $(document).ready(function () { // keep this at top
  //added>img for image listners on the child image elements not the parent div. this is a standard way of achieving the results
 //$("p") gives you all p elements, so$("audio") will give you all audio elements. but to make that work you need an id to call it
  $('.albumcovers>img').click(function() {
+       $(".cover").slideToggle();
+
    // calls al images in the class for the function
 	 // but first, hide all the divs to ensure that
 	 // only one will be open, ever
@@ -15,32 +17,33 @@ $(document).ready(function () { // keep this at top
 	 nextSong()
 
 	 // we can get the ID of the thing that was clicked -
-	 // switch ($(this).attr('id')) {
+	 switch ($(this).attr('id')) {
 
-	 // 	case '1':
-	 // 	$('#albums1').show();
-	 // 	break;
-	 // 	case '2':
-	 // 	$('#albums2').show();
-	 // 	break;
-	 // 	case '3':
-	 // 	$('#albums3').show();
-	 // 	break;
-	 // 	case '4':
-	 // 	$('#albums4').show();
-	 // 	break;
+	 case '1':
+	 $('#albums1').show();
+	 break;
+	 case '2':
+	 $('#albums2').show();
+	 break;
+	 case '3':
+	 $('#albums3').show();
+	 break;
+	 case '4':
+	 $('#albums4').show();
+	 break;
 
-	 // };
+	 };
 
 	 //the entire switch statement here instead. other one was wonky. Used the id to call the audio in the function
    // parent in the function accesses the DOM
-	 $("audio#soundtrack"+$(this).attr('id')).parent().show()
+	// $("audio#soundtrack"+$(this).attr('id')).parent().show()
 
 	});
 // end of function for clicking'
 
 // function to hide all divs
 function hideAll() {
+
 	$('#albums1').hide();
 	$('#albums2').hide();
 	$('#albums3').hide();
@@ -49,6 +52,7 @@ function hideAll() {
 };
 
 function nextSong(){
+;
 	$('audio').each(function(){
 		this.pause(); // Stop playing
 		this.currentTime = 0; // Reset time , comment it out if I don't want that song to start over again
